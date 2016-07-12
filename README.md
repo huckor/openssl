1b4e000ccd6341e758c8077ec3708cf57352bfc3
Robert Hucko <robo@hucko.sk>
2016-07-12 15:43:40 +0700

    Improved Eclipse project

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-C++ Openssl networking Demo.
+### Networking demo using Openssl library.
 
-Support secure and unsecure communication.
 
-Compatibility: OSX.
 
-Projects: Xcode
+Programming language: C++
+
+OS Usage: MacOS 64bit, Windows 32bit, Linux 64bit
+
+Projects: Xcode, Visual Studio 2010, Eclipse
+
+**Notes**
+
+Before run, set correct paths to certificate files. Look to main.cpp file to variable CertPath.  
+
+**What is this**
+
+This simple demo demonstrate how to use simple TCP/IP un-secure server - client communication and TCP/IP secure server - client communication. In both cases this application use Openssl library.
+Because this is just demo application, it can serve only one client at a time in both cases.
+
+**In nutshell this application doing this**
+
+After successful build and run, this application initialise Openssl library, create thread with server wait few seconds, then create client (in main thread) connect to server send 4bytes of data and close connection. This happened 2 times, first time using un-secure connection and second time using secure connection.
--- pro/Eclipse/Debug/subdir.mk
+++ pro/Eclipse/Debug/subdir.mk
@@ -44,70 +44,70 @@ CPP_DEPS += \
 Conv.o: /home/robo/workspace/Openssl/src/Conv.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Log.o: /home/robo/workspace/Openssl/src/Log.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Path.o: /home/robo/workspace/Openssl/src/Path.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 SslClient.o: /home/robo/workspace/Openssl/src/SslClient.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 SslServer.o: /home/robo/workspace/Openssl/src/SslServer.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 TcpIpClient.o: /home/robo/workspace/Openssl/src/TcpIpClient.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 TcpIpServer.o: /home/robo/workspace/Openssl/src/TcpIpServer.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Thread.o: /home/robo/workspace/Openssl/src/Thread.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Time.o: /home/robo/workspace/Openssl/src/Time.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 main.o: /home/robo/workspace/Openssl/src/main.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/robo/workspace/Openssl/dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../../dep/openssl/include -O0 -g3 -Wall -c -fmessage-length=0 -Wl,--no-as-needed -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
